f489b16d5ab2a19bf43bca0d23fe1de07f19b5a9
Kealan <kabrahambusse@gmail.com>
2025-07-22 10:16:02 +0100

    set up routes

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,12 @@ const app = express();
 const sampleRoutes = require('./routes/sampleRoutes');
 const userRoutes = require('./routes/userRoutes');
 
+const authRoutes = require('./routes/authRoutes');
+
+
 app.use(express.json());
 app.use('/api/samples', sampleRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
 
 module.exports = app;
