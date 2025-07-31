dca0a7b65923a7830a2ca0eefd74b0e763377b0c
Kealan <kabrahambusse@gmail.com>
2025-07-31 13:39:09 +0100

    app upload routes added

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const sampleRoutes = require('./routes/sampleRoutes');
 const userRoutes = require('./routes/userRoutes');
-
+const uploadRoutes = require('./routes/uploadRoutes');
 const authRoutes = require('./routes/authRoutes');
 
 
@@ -10,5 +10,6 @@ app.use(express.json());
 app.use('/api/samples', sampleRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/uploads', uploadRoutes);
 
 module.exports = app;
