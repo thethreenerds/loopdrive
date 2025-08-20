41bac0ab6bd42ccc4a65d3950a2e1fb6c0073953
Kealan <kabrahambusse@gmail.com>
2025-08-20 10:55:03 +0100

    added code to serve uploaded files on dashboard later

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const sampleRoutes = require('./routes/sampleRoutes');
 const userRoutes = require('./routes/userRoutes');
@@ -7,6 +8,10 @@ const authRoutes = require('./routes/authRoutes');
 
 
 app.use(express.json());
+
+//adding line to serve uploaded files
+app.use('/uploads', express.static(path,join(__dirname, 'uploads')));
+
 app.use('/api/samples', sampleRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
