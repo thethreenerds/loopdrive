8a650c65eb9db3c447e53f010f24893b337ac2be
Kealan <kabrahambusse@gmail.com>
2025-08-21 10:50:09 +0100

    myupload added

--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import UploadSample from "../components/UploadSample";
+import myUploads from "../components/MyUploads";
 
 function Dashboard(){
     return (
         <div>
             <h1>Dashboard</h1>
             <UploadSample />
+            <myUploads />
         </div>
     )
 }
