a8e17c3fb4848b74b54fe797f5f7ef8bbf8fecfa
Kealan <kabrahambusse@gmail.com>
2025-08-27 14:48:54 +0100

    changes for routing

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -5,5 +5,5 @@ const verifyToken = require('../middleware/verifyToken');
 const { handleUpload, getMyUploads } = require('../controllers/uploadController');
 
 router.post('/upload', verifyToken, upload.single('audio'), handleUpload);
-router.get('/', verifyToken, getMyUploads);
+router.get('/my-uploads', verifyToken, getMyUploads);
 module.exports = router;
