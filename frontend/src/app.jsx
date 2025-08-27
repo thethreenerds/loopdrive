8a446b0d7bc2c647473ab8043190b22c2148fe7c
Kealan <kabrahambusse@gmail.com>
2025-08-27 14:17:59 +0100

    removed redundant link as i will have myuploads and upload function on same dashboard

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import UploadSample from "./components/UploadSample";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/upload" element={<UploadSample />} />
                 <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
         </Router>
