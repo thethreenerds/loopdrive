d25b57c9350aaf5fadea8943ffcecd3ad6b7f847
Kealan <kabrahambusse@gmail.com>
2025-08-29 10:01:29 +0100

    adding protected route so that only reach dashboard when logged in

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
                 <Route path="/login" element={<Login />} />
             </Routes>
         </Router>
