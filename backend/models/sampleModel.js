c593d7ece812d6c4fe7001171a01fe40e1592eb9
Kealan <kabrahambusse@gmail.com>
2025-08-29 13:09:09 +0100

    refactored sample model to allow filtering

--- backend/models/sampleModel.js
+++ backend/models/sampleModel.js
@@ -33,14 +33,42 @@ const saveSample = async (sample) => {
     ]);
 }
 
-const getSamplesByUser = async (user_id) => {
-  const [rows] = await db.query(
-    `SELECT id, original_name, filename, filepath, tags, genre, bpm, sample_key, is_public, created_at
-     FROM samples
-     WHERE user_id = ?
-     ORDER BY created_at DESC`,
-    [user_id]
-  );
+const getSamplesByUser = async (user_id, filters = {}) => {
+
+  let query = `
+    SELECT id, original_name, filename, filepath, tags, genre, bpm, sample_key, is_public, created_at
+    FROM samples
+    WHERE user_id = ?
+  `;
+
+  const params = [user_id];
+
+  //dynamically add filters if they exist
+
+  if(filters.genre) {
+    query += 'AND genre =?';
+    params.push(filters.genre);
+  }
+
+  if(filters.bpm) {
+    query += 'AND bpm =?';
+    params.push(filters.bpm);
+  }
+
+  
+  if(filters.sample_key) {
+    query += 'AND sample_key =?';
+    params.push(filters.sample_key);
+  }
+  
+  if (filters.tags) {
+    // LIKE filter for tags
+    query += ' AND tags LIKE ?';
+    params.push(`%${filters.tags}%`);
+  }
+
+  query += 'ORDER BY created_at DESC';
+  const [rpws] = await db.query(query, params);
   return rows;
 };
 
